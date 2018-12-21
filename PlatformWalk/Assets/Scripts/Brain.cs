ad4126fd6ecc134bd04e82e51e40137ed76f685a
Ishmaelk <Kheir1@msn.com>
2018-12-21 10:21:49 -0500

    Wrote DNA, Brain, and Population Manager scripts

---
+++ PlatformWalk/Assets/Scripts/Brain.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+    int dnaLength = 2;
+    public float timeAlive;
+    public DNA dna;
+    public GameObject eyes;
+    bool alive = true;
+    bool groundVisible = true;
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("dead"))
+            alive = false;
+    }
+
+    public void Init () {
+        // forward: 0, left: 1, right: 2
+        dna = new DNA(dnaLength, 3);
+        timeAlive = 0;
+        alive = true;
+    }
+
+	void Update () {
+        if (!alive) return;
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
+        
+        RaycastHit hit;
+        if (Physics.Raycast(eyes.transform.position, eyes.transform.forward*10, out hit))
+            groundVisible = hit.collider.gameObject.CompareTag("platform") ? true : false;
+        timeAlive = PopulationManager.elapsed;
+        float turn = 0;
+        float move = 0;
+        if (groundVisible) {
+            int zeroGene = dna.GetGene(0);
+            if (zeroGene == 0) move = 1;
+            else if (zeroGene == 1) turn = -90;
+            else if (zeroGene == 2) turn = 90;
+        } else { // if the ground is not visible
+            int oneGene = dna.GetGene(1);
+            if (oneGene == 0) move = 1;
+            else if (oneGene == 1) turn = -90;
+            else if (oneGene == 2) turn = 90;
+        }
+
+        this.transform.Translate(0, 0, move * 0.1f);
+        this.transform.Rotate(0, turn, 0);
+
+	}
+}
---
+++ PlatformWalk/Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour {
+
+    public static float elapsed = 0;
+    public float trialTime = 5;
+    int generation = 1;
+
+    public GameObject BOTPREFAB;
+    public int populationSize = 50;
+    List<Brain> population = new List<Brain>();
+
+    GUIStyle guiStyle = new GUIStyle();
+
+    void OnGUI() {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population Size: " + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+   
+    void Start () { // create starting population
+        population = new List<Brain>();
+        BOTPREFAB = Resources.Load<GameObject>("Agent");
+        for (int i = 0; i < populationSize; i++) {
+            Vector3 start_pos = new Vector3(this.transform.position.x + Random.Range(-2, 2),
+                                            this.transform.position.y + Random.Range(-2, 2),
+                                            this.transform.position.z + 0.5f);
+            GameObject agent = Instantiate(BOTPREFAB, start_pos, this.transform.rotation);
+            Brain brain = agent.GetComponent<Brain>();
+            brain.Init();
+            population.Add(brain);
+        }
+	}
+
+    void Update() {
+        elapsed += Time.deltaTime;
+        if (elapsed >= trialTime) {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+    }
+
+    void BreedNewPopulation () {
+        List<Brain> sorted = population.OrderBy(o => o.timeAlive).ToList();
+        population.Clear();
+        for (int i = sorted.Count/2; i < sorted.Count-1; i++) { // breed best half of population
+            population.Add(Breed(sorted[i], sorted[i + 1]));
+            population.Add(Breed(sorted[i+1], sorted[i]));
+        }
+
+        foreach(var a in population)
+            Destroy(a.gameObject);
+        generation++;
+
+    }
+
+    Brain Breed (Brain parent1, Brain parent2) {
+        Vector3 start_pos = new Vector3(this.transform.position.x + Random.Range(-2, 2),
+                                            this.transform.position.y + Random.Range(-2, 2),
+                                            this.transform.position.z + 0.5f);
+        GameObject offspring = Instantiate(BOTPREFAB, start_pos, this.transform.rotation);
+        Brain brain = offspring.GetComponent<Brain>();
+        brain.Init();
+        if (Random.Range(0, 100) == 1)
+            brain.dna.Mutate();
+        else
+            brain.dna.Combine(parent1.dna, parent2.dna);
+        return brain;
+    }
+	
+	
+
+
+	void Update () {
+		
+	}
+}
