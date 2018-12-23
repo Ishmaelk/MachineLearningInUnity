7d7bcc02bbfbb50de4bd3f4f6b79e05d8e727d43
Ishmaelk <Kheir1@msn.com>
2018-12-23 13:03:11 -0500

    debugged ANN finished

--- ArtificialNeuralNetwork/Assets/Scripts/Brain.cs
+++ ArtificialNeuralNetwork/Assets/Scripts/Brain.cs
@@ -27,13 +27,13 @@ public class Brain : MonoBehaviour {
         Debug.Log("Sum of squared error: " + sumSquareError);
         Debug.Log("---------------------");
         result = Train(1, 1, 0);
-        Debug.Log("XOR(1, 1) = " + result[0]);
+        Debug.Log("XOR(1, 1) = " + Mathf.RoundToInt((float)result[0]));
         result = Train(0, 1, 1);
-        Debug.Log("XOR(0, 1) = " + result[0]);
+        Debug.Log("XOR(0, 1) = " + Mathf.RoundToInt((float)result[0]));
         result = Train(1, 0, 1);
-        Debug.Log("XOR(1, 0) = " + result[0]);
+        Debug.Log("XOR(1, 0) = " + Mathf.RoundToInt((float)result[0]));
         result = Train(0, 0, 0);
-        Debug.Log("XOR(0, 0) = " + result[0]);
+        Debug.Log("XOR(0, 0) = " + Mathf.RoundToInt((float)result[0]));
     }
 	
     List<double> Train (double i1, double i2, double o) {
