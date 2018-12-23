7e9544a6be0303e0d78a262faf704b4276d34538
Ishmaelk <Kheir1@msn.com>
2018-12-23 11:31:50 -0500

    created neuron and layer classes

---
+++ ArtificialNeuralNetwork/Assets/Scripts/Layer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer { // holds the neurons for this particular layer
+
+    public int numNeurons;
+    public List<Neuron> neurons = new List<Neuron>();
+
+    public Layer (int num_neurons, int num_neuron_inputs) { // number of inputs is the number of neurons in previous layer
+        numNeurons = num_neurons;
+        for (int i = 0; i < num_neurons; i++)
+            neurons.Add(new Neuron(num_neuron_inputs));
+    }
+
+	
+}
---
+++ ArtificialNeuralNetwork/Assets/Scripts/Neuron.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron { // same functionality as perceptron before
+
+    public int numInputs; // use this to create number of weights
+    public double bias;
+    public double output;
+    public double errorGradient; // //
+    public List<double> weights = new List<double>();
+    public List<double> inputs = new List<double>();
+
+    public Neuron (int ninput) {
+        bias = Random.Range(-1.0f, 1.0f);
+        numInputs = ninput;
+        for (int i = 0; i < numInputs; i++)
+            weights.Add(Random.Range(-1.0f, 1.0f));
+    }
+
+}
