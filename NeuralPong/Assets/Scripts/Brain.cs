c54162d60ac2b30dd5d39ad23b73e19587b0c1da
Ishmaelk <Kheir1@msn.com>
2018-12-25 12:00:38 -0500

    added NeuralPong project

---
+++ NeuralPong/Assets/Scripts/Brain.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+    public GameObject paddle;
+    public GameObject ball;
+    Rigidbody2D brb;
+    float yvel;
+    float paddleMinY;
+    float paddleMaxY;
+    float paddleMaxSpeed;
+    public float numSaved;
+    public float numMissed;
+    ANN ann;
+
+	void Start () {
+        paddleMinY = 8.8f;
+        paddleMaxY = 17.4f;
+        paddleMaxSpeed = 15;
+        numSaved = 0;
+        numMissed = 0;
+        brb = ball.GetComponent<Rigidbody2D>();
+        ann = new ANN(6, 1, 1, 4, 0.11); // 6 inputs, 1 output, 1 hidden layer, 4 neurons, 0.11 alpha
+    }
+	
+	void Update () {
+        float posy = Mathf.Clamp(paddle.transform.position.y + (yvel * Time.deltaTime * paddleMaxSpeed),
+            paddleMinY, paddleMaxY);
+        Vector3 position = new Vector3 ( paddle.transform.position.x, posy, paddle.transform.position.z );
+        paddle.transform.position = position;
+        List<double> output = new List<double>();
+        int layerMask = 1 << 9;
+        RaycastHit2D hit = Physics2D.Raycast(ball.transform.position, brb.velocity, 1000, layerMask);
+        if (hit.collider != null) {
+            if (hit.collider.gameObject.CompareTag("tops")) { // raycast from reflection to backwall
+                Vector3 reflection = Vector3.Reflect(brb.velocity, hit.normal);
+                hit = Physics2D.Raycast(hit.point, reflection, 1000, layerMask);
+            }
+            if (hit.collider == null) {
+                yvel = 0;
+                return;
+            }
+            if (gameObject.name == "RightPlayerBrain" &&
+                hit.collider.gameObject.CompareTag("backwall")) { // raycast from ball -> ball direction
+                float dy = hit.point.y - paddle.transform.position.y; // get difference in y from paddle to backwall hit
+                output = Run(ball.transform.position.x,
+                            ball.transform.position.y,
+                            brb.velocity.x, brb.velocity.y,
+                            paddle.transform.position.x,
+                            paddle.transform.position.y,
+                            dy, true); // train the ANN
+                yvel = (float)output[0]; // set the y to the output
+            } else if (gameObject.name == "LeftPlayerBrain"
+                && hit.collider.gameObject.CompareTag("frontwall")) {
+                float dy = hit.point.y - paddle.transform.position.y; // get difference in y from paddle to backwall hit
+                output = Run(ball.transform.position.x,
+                            ball.transform.position.y,
+                            brb.velocity.x, brb.velocity.y,
+                            paddle.transform.position.x,
+                            paddle.transform.position.y,
+                            dy, true); // train the ANN
+                yvel = (float)output[0]; // set the y to the output
+            }
+        } else // otherwise paddle doesnt move
+            yvel = 0;
+	}
+
+    // Wrapper method for train and calculate output //
+    List<double> Run (double bx, double by, double bvx, double bvy, double px, double py, double pv, bool train) {
+        List<double> inputs = new List<double>() { bx, by, bvx, bvy, px, py };
+        List<double> outputs = new List<double>() { pv };
+        return train ? ann.Train(inputs, outputs) : ann.CalcOutput(inputs, outputs);  
+    }
+
+}
---
+++ NeuralPong/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+    Vector3 ballStartPosition;
+    Rigidbody2D rb;
+    float speed = 400;
+    public AudioSource blip;
+    public AudioSource blop;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        ballStartPosition = transform.position;
+        ResetBall();
+	}
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) ResetBall();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("backwall")) blop.Play();
+        else blip.Play();
+    }
+
+    public void ResetBall() {
+        transform.position = ballStartPosition;
+        rb.velocity = Vector3.zero;
+        Vector3 dir = new Vector3(Random.Range(100, 300), Random.Range(-100, 100), 0).normalized;
+        rb.AddForce(dir * speed);
+    }
+
+
+    
+}
